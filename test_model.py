4c24e6aa5fbb750b03aeca51e895b717cdea8fcf
KageBinary <ianjakobhock@gmail.com>
2026-04-13 14:19:00 -0700

    fix int8 tflite: remove float32 io override, fix test script dtype

---
+++ test_model.py
@@ -0,0 +1,24 @@
+import tflite_runtime.interpreter as tflite
+import numpy as np
+
+interpreter = tflite.Interpreter(
+    model_path='/root/imx-ecg/artifacts/ecg_deploy_int8.tflite',
+    experimental_delegates=[tflite.load_delegate('/usr/lib/libvx_delegate.so')]
+)
+interpreter.allocate_tensors()
+
+inp = interpreter.get_input_details()
+out = interpreter.get_output_details()
+print('Input shape:', inp[0]['shape'], 'dtype:', inp[0]['dtype'])
+print('Output shape:', out[0]['shape'], 'dtype:', out[0]['dtype'])
+
+# Input is [1, 3000, 1] after onnx2tf NHWC transpose
+input_dtype = inp[0]['dtype']
+print('Input dtype:', input_dtype)
+dummy = np.zeros(inp[0]['shape'], dtype=input_dtype)
+interpreter.set_tensor(inp[0]['index'], dummy)
+interpreter.invoke()
+result = interpreter.get_tensor(out[0]['index'])
+print('Output logits:', result)
+print('Predicted class:', result.argmax())
+print('Classes: 0=Normal 1=AF 2=Other 3=Noisy')
