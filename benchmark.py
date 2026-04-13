69fee2f35bad3562d39400dc9a715cd09bb02c46
KageBinary <ianjakobhock@gmail.com>
2026-04-13 14:34:05 -0700

    fix benchmark: handle int8 input dtype

--- benchmark.py
+++ benchmark.py
@@ -4,23 +4,32 @@ import time
 
 model_path = '/root/imx-ecg/artifacts/ecg_deploy_int8.tflite'
 
+def make_dummy(interp):
+    inp = interp.get_input_details()[0]
+    dummy_f32 = np.zeros(inp['shape'], dtype=np.float32)
+    if inp['dtype'] == np.int8:
+        scale, zero_point = inp['quantization']
+        return (dummy_f32 / scale + zero_point).astype(np.int8)
+    return dummy_f32
+
 # With NPU delegate
 interp_npu = tflite.Interpreter(
     model_path=model_path,
     experimental_delegates=[tflite.load_delegate('/usr/lib/libvx_delegate.so')]
 )
 interp_npu.allocate_tensors()
-inp = interp_npu.get_input_details()
-dummy = np.zeros(inp[0]['shape'], dtype=np.float32)
-interp_npu.set_tensor(inp[0]['index'], dummy)
+dummy_npu = make_dummy(interp_npu)
+interp_npu.set_tensor(interp_npu.get_input_details()[0]['index'], dummy_npu)
 t0 = time.perf_counter()
 for _ in range(50):
     interp_npu.invoke()
 npu_ms = (time.perf_counter() - t0) / 50 * 1000
+
 # Without delegate (CPU only)
 interp_cpu = tflite.Interpreter(model_path=model_path)
 interp_cpu.allocate_tensors()
-interp_cpu.set_tensor(interp_cpu.get_input_details()[0]['index'], dummy)
+dummy_cpu = make_dummy(interp_cpu)
+interp_cpu.set_tensor(interp_cpu.get_input_details()[0]['index'], dummy_cpu)
 t0 = time.perf_counter()
 for _ in range(50):
     interp_cpu.invoke()
